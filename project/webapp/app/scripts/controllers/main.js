9740dd7ef732b5ea4f30e2c8f2eef76157b00d63
zhuchenshu <1369359429@qq.com>
2017-10-21 22:39:17 +0800

    2.7.1

--- project/webapp/app/scripts/controllers/main.js
+++ project/webapp/app/scripts/controllers/main.js
@@ -9,28 +9,32 @@
  */
 angular.module('testApp')
   .controller('MainCtrl', function($scope, $http) {
-    var print = function() {
-        console.info($scope.helloWorld, $scope.hi);
+    var success = function(response) {
+      $scope.helloWorld = response.data;
+      $scope.hi = $scope.helloWorld + ' Hi!';
+    };
+
+    var error = function(response) {
+      console.error('$http -> ' + url + ' error.', response);
     };
 
     var url = 'http://localhost:9000/data/helloWorld.txt';
-    console.log('before handle $http.get().then()');
-    print();
     $http.get(url)
-    .then(function success(response){
-        console.info('print success response', response);
-        print();
-        $scope.helloWorld = response.data;
-        print();
-    }, function error(response){
-        console.error('$http -> ' + url + ' error.', response);
-    });
-    console.log('after handle $http.get().then()');
-    print();
+      .then(success, error);
 
-    console.log('before handle $scope.hi');
-    print();
-    $scope.hi = $scope.helloWorld + ' Hi!';
-    console.log('after handle $scope.hi');
-    print();
+    var thenTest = function(successCallback, errorCallback) {
+        var successHello = 'hello success callback';
+        var errorHello = 'hello error callback';
+        successCallback(successHello);
+        errorCallback(errorHello);
+    };
+    var successFunction = function(hello) {
+        console.info('sucess recive hello:', hello);
+    };
+
+    var errorFunction = function(hello) {
+        console.info('error revice hello: ', hello);
+    };
+
+    thenTest(successFunction, errorFunction);
   });
