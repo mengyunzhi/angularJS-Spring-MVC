27867f50b247d25fba9b0f4a53b1c579998c047f
zhuchenshu <1369359429@qq.com>
2017-10-21 22:27:39 +0800

    2.6

--- project/webapp/app/scripts/controllers/main.js
+++ project/webapp/app/scripts/controllers/main.js
@@ -8,8 +8,29 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function($scope) {
-    $scope.helloWorld = 'Hello World!';
+  .controller('MainCtrl', function($scope, $http) {
+    var print = function() {
+        console.info($scope.helloWorld, $scope.hi);
+    };
+
+    var url = 'http://localhost:9000/data/helloWorld.txt';
+    console.log('before handle $http.get().then()');
+    print();
+    $http.get(url)
+    .then(function success(response){
+        console.info('print success response', response);
+        print();
+        $scope.helloWorld = response.data;
+        print();
+    }, function error(response){
+        console.error('$http -> ' + url + ' error.', response);
+    });
+    console.log('after handle $http.get().then()');
+    print();
+
+    console.log('before handle $scope.hi');
+    print();
     $scope.hi = $scope.helloWorld + ' Hi!';
-    console.log($scope.helloWorld);
+    console.log('after handle $scope.hi');
+    print();
   });
