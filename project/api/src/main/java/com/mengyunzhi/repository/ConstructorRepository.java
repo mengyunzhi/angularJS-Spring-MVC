4994ffff2f3575c76832c7c373b7f5c47ca8a775
zhuchenshu <1369359429@qq.com>
2017-10-22 21:47:07 +0800

    3.2.5-link-table-and-data-init

---
+++ project/api/src/main/java/com/mengyunzhi/repository/ConstructorRepository.java
@@ -0,0 +1,6 @@
+package com.mengyunzhi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConstructorRepository extends CrudRepository<Constructor, Long> {
+}
