4287cec5b81eb7e060e2f294b8ac068f57544f7a
zhuchenshu <1369359429@qq.com>
2017-10-22 22:03:45 +0800

    3.2.7-front-to-back

--- project/webapp/app/scripts/controllers/main.js
+++ project/webapp/app/scripts/controllers/main.js
@@ -8,21 +8,12 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function($scope) {
-    $scope.data = [
-      {
-        id: 1,
-        name: 'zhangsan',
-        sex: 0,
-        email: 'zhangsan@mengyunzhi.com',
-        username: 'zhangsan'
-      },
-      {
-        id: 2,
-        name: 'lisi',
-        sex: 1,
-        email: 'lisi@mengyunzhi.com',
-        username: 'lisi'
-      }
-    ];
+  .controller('MainCtrl', function($scope, $http) {
+    var url = 'http://localhost:8080/Constructor/';
+    $http.get(url)
+    .then(function success(response){
+      $scope.data = response.data;
+    }, function error(response) {
+      console.error('url:' + url, response);
+    });
   });
