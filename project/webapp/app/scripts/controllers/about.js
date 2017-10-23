d9786926e3c35a1dd9245b9fb649d543c0a7a513
zhuchenshu <1369359429@qq.com>
2017-10-23 21:09:17 +0800

    3.3.3-webapp-controller

--- project/webapp/app/scripts/controllers/about.js
+++ project/webapp/app/scripts/controllers/about.js
@@ -8,10 +8,12 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('AboutCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function($scope) {
+    $scope.object = {
+      id: 1,
+      name: 'zhangsan',
+      email: 'zhangsan@mengyunzhi.com',
+      sex: 0,
+      username: 'zhangsan'
+    };
   });
