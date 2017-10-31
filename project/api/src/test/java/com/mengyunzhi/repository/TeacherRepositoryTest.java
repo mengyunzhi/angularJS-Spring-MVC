0faeba559a87cd9d662e5d4c4d4fe0ed54372fe2
zhuchenshu <1369359429@qq.com>
2017-10-31 22:00:42 +0800

    3.2.5-api-link-table-and-data-init

--- project/api/src/test/java/com/mengyunzhi/repository/TeacherRepositoryTest.java
+++ project/api/src/test/java/com/mengyunzhi/repository/TeacherRepositoryTest.java
@@ -3,7 +3,6 @@ package com.mengyunzhi.repository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,14 +13,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class ConstructorRepositoryTest {
+public class TeacherRepositoryTest {
 
     @Autowired
-    private ConstructorRepository constructorRepository;
+    private TeacherRepository constructorRepository;
 
     @Test
     public void findAllTest() {
-        List<Constructor> constructors = (List<Constructor>) constructorRepository.findAll();
+        List<Teacher> constructors = (List<Teacher>) constructorRepository.findAll();
         assertThat(constructors.size()).isEqualTo(2);
     }
 
