f939b3716624afd0cd2f9a78f172b364f9879f43
chenzhigao <2934472815.com>
2017-11-04 15:12:05 +0800

    修改分支

--- project/webapp/app/scripts/controllers/main.js
+++ project/webapp/app/scripts/controllers/main.js
@@ -9,28 +9,47 @@
  */
 angular.module('testApp')
   .controller('MainCtrl', function($scope, $http) {
+
+    // 定义一个函数，用于打印输出值
     var print = function() {
-        console.info($scope.helloWorld, $scope.hi);
+      console.info($scope.helloWorld, $scope.hi);
     };
 
-    var url = 'http://localhost:9000/data/helloWorld.txt';
-    console.log('before handle $http.get().then()');
-    print();
-    $http.get(url)
-    .then(function success(response){
-        console.info('print success response', response);
-        print();
-        $scope.helloWorld = response.data;
-        print();
-    }, function error(response){
-        console.error('$http -> ' + url + ' error.', response);
-    });
-    console.log('after handle $http.get().then()');
+    // 打印未获取数据之前的结果
+    console.log('1.未通过$http.get()获取数据之前打印结果：');
     print();
 
-    console.log('before handle $scope.hi');
+    // 通过路由获取传过来的数据
+    var url = 'http://localhost:9000/data/helloWorld.txt';
+    $http.get(url)
+      .then(function success(response) {
+
+        // 打印获取结果和查看传过来的数据
+        console.info('2.成功获取数据！但没有对数据进行操作。', response);
+        print();
+
+        // 接收传过来的值
+        $scope.helloWorld = response.data;
+
+        // 打印传过来后并赋值后的结果
+        console.info('3.进行赋值操作后的打印结果：');
+        print();
+      }, function error(response) {
+        console.error('$http -> ' + url + ' error.', response);
+      });
+
+    // 执行完我们认为执行完的$http.get().then()后
+    console.log('4.执行完$http.get().then()操作后：');
     print();
+
+    // 打印未对$scope.hi操作之前的结果
+    console.log('5.在对$scope.hi操作之前：');
+    print();
+
+    // 对 $scope.hi 进行赋值
     $scope.hi = $scope.helloWorld + ' Hi!';
-    console.log('after handle $scope.hi');
+
+    // 打印对 $scope.hi 进行操作之后的结果
+    console.log('6.在对$scope.hi操作之后：');
     print();
   });
